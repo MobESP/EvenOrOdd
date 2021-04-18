2157a749cc470429c94bd7aa3675448173e14146
MobESP <51514619+MobESP@users.noreply.github.com>
2021-04-17 22:23:53 -0400

    Added the main script

---
+++ EvenOrOdd.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
+}
+
+// Program is supposed to take the integer that you put as input and determine whether it is even or odd.
